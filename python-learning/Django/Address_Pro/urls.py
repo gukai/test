2e6cd8160fd54cfa94baeebdbfa68ed8321e44de
gukai <307475423@qq.com>
2013-11-05 06:36:41 +0000

    build Address project.

---
+++ python-learning/Django/Address_Pro/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Address_Pro.views.home', name='home'),
+    # url(r'^Address_Pro/', include('Address_Pro.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'Users.add.index', name='home'),
+)
